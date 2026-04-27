56fd2fd0d25b9b3c849f305931c751acc98ef786
MorizZatko <moriz@zatko.de>
2026-04-27 23:12:31 +0200

    rename and added .py to filename

---
+++ Rendering_local_vs_cloud.py
@@ -0,0 +1,48 @@
+"""
+Module: Rendering Price Comparison
+Description: Module comparse local rendering costs with cloud rendering costs.
+Function:
+    1. Asks user for local costs per hour and rendering time in hours, same for the cloud costs and time.
+    2. Calculates local and cloud costs aswell as the difference between those.
+    3. Checks wich option costs less and wich is faster.
+    4. Outputs a string chart to compare cost, time and difference. 
+       Additional it outputs two recommendation strings for the fastest and the cheapest option.
+
+Args:
+    None:
+"""
+
+
+local_price = float(input("Lokale Stromkosten pro Stunde:").replace(",", "."))
+local_time = float(input("Lokale Render Zeit in Stunden:").replace(",", "."))
+cloud_price = float(input("Cloud Stromkosten pro Stunde:").replace(",", "."))
+cloud_time = float(input("Cloud Render Zeit in Stunden:").replace(",", "."))
+
+# --- Calculate logic ---
+local_cost = local_price * local_time
+cloud_cost = cloud_price * cloud_time
+dif_cost = abs(cloud_cost - local_cost)
+dif_time  = abs(local_time - cloud_time) 
+
+if local_cost < cloud_cost:
+    tip = "Lokal"
+else:
+    tip = "Cloud"
+
+if local_time < cloud_time:
+    tip_time = "Lokal"
+else: 
+    tip_time = "Cloud"
+
+# --- Output ---
+print("+" + "-"*53 + "+")
+print(f"| {"Anbieter":<15} | {"Kosten in €":>15} | {"Zeit in h":>15} |")
+print("+" + "-"*53 + "+")
+print(f"| {"Lokal":<15} | {local_cost:>15.2f} | {local_time:>15.2f} |")
+print("+" + "-"*53 + "+")
+print(f"| {"Cloud":<15} | {cloud_cost:>15.2f} | {cloud_time:>15.2f} |")
+print("+" + "-"*53 + "+")
+print(f"| {"Differenz":<15} | {dif_cost:>15.2f} | {dif_time:>15.2f} |")
+print("+" + "-"*53 + "+")
+print(f"\nDie Kostengünstigere Variante ist {tip}, um {dif_cost:.2f}€ günstiger.")
+print(f"Die Schnellere Variante ist {tip_time}, um {dif_time:.2f}h schneller.")
