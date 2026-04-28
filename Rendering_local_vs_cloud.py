f3b40973d8965d93d63762806231158b44e9cc4c
MorizZatko <Moriz@zatko.de>
2026-04-28 12:04:28 +0200

    corrected docstrings and comments

--- Rendering_local_vs_cloud.py
+++ Rendering_local_vs_cloud.py
@@ -1,29 +1,24 @@
-"""
-Module: Rendering Price Comparison
-Description: Module comparse local rendering costs with cloud rendering costs.
-Function:
-    1. Asks user for local costs per hour and rendering time in hours, same for the cloud costs and time.
-    2. Calculates local and cloud costs aswell as the difference between those.
-    3. Checks wich option costs less and wich is faster.
-    4. Outputs a string chart to compare cost, time and difference. 
-       Additional it outputs two recommendation strings for the fastest and the cheapest option.
+"""Rendering time and price comparison.
 
-Args:
-    None:
+This module calculates and compares local and cloud rendering time and costs by values
+provided via standard user input.
+Outputs a chart via standard output, displaying the costs/time aswell as the differences (euro and hours).
+Outputs two strings with the fastest and the cheapest option.
 """
 
-
+# User input
 local_price = float(input("Lokale Stromkosten pro Stunde:").replace(",", "."))
 local_time = float(input("Lokale Render Zeit in Stunden:").replace(",", "."))
 cloud_price = float(input("Cloud Stromkosten pro Stunde:").replace(",", "."))
 cloud_time = float(input("Cloud Render Zeit in Stunden:").replace(",", "."))
 
-# --- Calculate logic ---
+# Calculates time/price and difference
 local_cost = local_price * local_time
 cloud_cost = cloud_price * cloud_time
 dif_cost = abs(cloud_cost - local_cost)
 dif_time  = abs(local_time - cloud_time) 
 
+# Logic to detect fastest and cheapest option
 if local_cost < cloud_cost:
     tip = "Lokal"
 else:
@@ -34,7 +29,7 @@ if local_time < cloud_time:
 else: 
     tip_time = "Cloud"
 
-# --- Output ---
+# Outputs string chart
 print("+" + "-"*53 + "+")
 print(f"| {"Anbieter":<15} | {"Kosten in €":>15} | {"Zeit in h":>15} |")
 print("+" + "-"*53 + "+")
@@ -44,5 +39,7 @@ print(f"| {"Cloud":<15} | {cloud_cost:>15.2f} | {cloud_time:>15.2f} |")
 print("+" + "-"*53 + "+")
 print(f"| {"Differenz":<15} | {dif_cost:>15.2f} | {dif_time:>15.2f} |")
 print("+" + "-"*53 + "+")
+
+# Outputs string for smart decision
 print(f"\nDie Kostengünstigere Variante ist {tip}, um {dif_cost:.2f}€ günstiger.")
 print(f"Die Schnellere Variante ist {tip_time}, um {dif_time:.2f}h schneller.")
