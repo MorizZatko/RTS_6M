4e2c4b16444b48b1b91f76a8c02d66180565ac53
MorizZatko <Moriz@zatko.de>
2026-04-28 10:50:07 +0200

    corrected docstrings and comments

--- Rendertime_Calculator.py
+++ Rendertime_Calculator.py
@@ -1,34 +1,33 @@
-"""
-Module: Redering Time/Price Calculator
-Description: Tool that calculates the needed rendering time and price trough given values via user input.
-Function:
-    1. Asks user for the project name (project), total number of frames (number_of_frames), time per frame in seconds (time_per_frame)
-       and the rendering price per minute (price_per_minute)
-    2. Calculates the rendering time in minutes and hours aswell as the price for the rendering in euro.
-    3. Outputs strings with all results for the project.
-    4. Request the user to press enter to close this module.
+"""Redering Time/Price Calculator.
 
-Args:
-    None: All needed values are provided by the user via standard input.
-Retuns:
-    None: Outputs four strings with all final results via standard output.
+This Module calculates the needed rendering time and price by values provided via standard user input.
+Output needed time in in seconds, minutes and hours aswell as the total price for the rendering.
 """
 
+# User input
 project = input("Projektname:")
 number_of_frames = int(input("Anzahl der Frames:"))
 time_per_frame = float(input("Zeit pro Frame in Sekunden:").replace(",", "."))
 price_per_minute = float(input("Renderpreis pro Minute:").replace(",", "."))
 
-# --- Calculate logic ---
+# LOGIC BLOCK
+# 1. Calculates needed time
 time = number_of_frames * time_per_frame
+
+# 2. Calculates time in minutes
 time_minute = time / 60
+
+# 3. Calculates time in hours
 time_hour = time_minute / 60
+
+# 4. Calculates render price
 render_price = price_per_minute * time_minute
 
-# --- Output ---
+# Output all results
 print(f"---Die Renderzeit für das Projekt {project}, beträgt vorraussichtlich {time:.2f} Sekunden---")
 print(f"In Minuten: {time_minute:.2f}")
 print(f"In Stunden: {time_hour:.2f}")
 print(f"Die Kosten liegen bei ca. {render_price:.2f}€")
 
+# Request user to end programm
 input("\nDrücke Enter, um das Programm zu beenden...")
