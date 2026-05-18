db6e94b6e0462ae75b48676cdc74ef149ff54f6a
MorizZatko <Moriz@zatko.de>
2026-05-18 09:22:42 +0200

    transfered code into function

--- Pillow_Training_Task_2.py
+++ Pillow_Training_Task_2.py
@@ -5,6 +5,11 @@ This modul take a image file and resize it to 512x512 px.
 from PIL import Image
 
 img = Image.open('Media_Doc/asset_1.png')
-img_resize = img.resize((512, 512), resample=Image.LANCZOS)
-img_resize.save('Media_Doc/asset_1_resize.png')
-img_resize.show()
+
+def resize_image(img):
+    """Function to resize image by using LANCZOS algorythm."""
+    img_resize = img.resize((512, 512), Image.Resampling.LANCZOS)
+    img_resize.save('Media_Doc/asset_1_resize.png')
+    return img_resize
+
+resize_image(img)
