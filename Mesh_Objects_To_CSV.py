735967f8740ab9eef611a4e1b16fc1ec8f96a31c
MorizZatko <Moriz@zatko.de>
2026-07-01 10:44:43 +0200

    changed from a nested list to a list with dictionaries, to enable more complex data structure

--- Mesh_Objects_To_CSV.py
+++ Mesh_Objects_To_CSV.py
@@ -1,7 +1,7 @@
 """Mesh objects to CSV list.
 
 This module iterates through all objects in the Blender scene to find mesh objects.
-It fills a nested list with the collected data. 
+It fills a list with dictionaries containing the collected data. 
 Finally, it writes all values to a CSV file at a hardcoded file path.
 (Notice: The code must run within an existing Blender project.)
 """
@@ -12,16 +12,17 @@ import csv
 # Initialize list
 assets = []
 
-# Find all mesh objects and append it to a nested list
+# Find all mesh objects and append their data as dictionaries to the list
 for obj in bpy.data.objects:
     if obj.type == 'MESH':
         name = obj.name
         count = len(obj.data.vertices)
         type = 'Mesh'
-        assets.append([name, count, type])
+        assets.append({"Object Name": name, "Vertices": count, "Type": type})
 
 # Create CSV list with all found values
 with open('C://Users//moriz//Desktop//RTS//python_start//assets.csv', 'w', newline='') as f:
-    writer = csv.writer(f)
-    writer.writerow(['Object Name', 'Vertices', 'Type'])
+    head_field = ['Object Name', 'Vertices', 'Type']
+    writer = csv.DictWriter(f, fieldnames=head_field)
+    writer.writeheader()
     writer.writerows(assets)
