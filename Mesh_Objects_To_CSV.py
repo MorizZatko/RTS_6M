e12fac47c797ba2503e3d6fe9cc9cc74948de73f
MorizZatko <Moriz@zatko.de>
2026-07-01 11:10:49 +0200

    changed from a nested list to a list with dictionaries, to enable more complex data structure

--- Mesh_Objects_To_CSV.py
+++ Mesh_Objects_To_CSV.py
@@ -1,13 +1,14 @@
-"""Mesh objects to CSV list.
+"""Mesh objects to a JSON file.
 
 This module iterates through all objects in the Blender scene to find mesh objects.
-It fills a list with dictionaries containing the collected data. 
-Finally, it writes all values to a CSV file at a hardcoded file path.
+All collected data is stored in a dictionary structure.
+Finally, it writes all values to a JSON file at a hardcoded file path.
 (Notice: The code must run within an existing Blender project.)
 """
 
 import bpy
-import csv
+import json
+import os
 
 # Initialize list
 assets = []
@@ -18,11 +19,17 @@ for obj in bpy.data.objects:
         name = obj.name
         count = len(obj.data.vertices)
         type = 'Mesh'
-        assets.append({"Object Name": name, "Vertices": count, "Type": type})
+        location = obj.matrix_world.translation
+        assets.append({"Object Name": name, "Vertices": count, "Type": type, "Location": [location.x, location.y, location.z]})
 
-# Create CSV list with all found values
-with open('C://Users//moriz//Desktop//RTS//python_start//assets.csv', 'w', newline='') as f:
-    head_field = ['Object Name', 'Vertices', 'Type']
-    writer = csv.DictWriter(f, fieldnames=head_field)
-    writer.writeheader()
-    writer.writerows(assets)
+# Initializing dictionary base structure with dynamic name and asset values     
+project_path = bpy.data.filepath
+project_name = os.path.basename(project_path)
+scene_data = {
+    "metadata": {"author": "Moriz", "version": 3.0},
+    "scene_name": project_name, "objects": assets
+    }
+
+# Create JSON file with all found values, stored as dictionaries
+with open('C://Users//moriz//Desktop//RTS//python_start//scene.json', 'w') as f:
+    json.dump(scene_data, f, indent=4)
